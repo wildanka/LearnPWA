c3377f8985a479479a8611136bc13de3e8c2d791
WildanKurniadi <danprogramming10@gmail.com>
2019-05-12 22:38:02 +0700

    #2 Latihan1 - Menambahkan Halaman untuk Kontent pada Shell

--- js/nav.js
+++ js/nav.js
@@ -19,4 +19,26 @@ document.addEventListener('DOMContentLoaded', function() {
     xhttp.open('GET', 'nav.html', true);
     xhttp.send();
   }
+
+  var page = window.location.hash.substr(1);
+  if (page == '') page = 'home';
+  loadPage();
+
+  function loadPage() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        var content = document.querySelector('#body-content');
+        if (this.status == 200) {
+          content.innerHTML = xhttp.responseText;
+        } else if (this.status == 404) {
+          content.innerHTML = '<p>Halaman tidak ditemukan</p>';
+        } else {
+          content.innerHTML = '<p>Ups.. Halaman tidak dapat diakses</p>';
+        }
+      }
+    };
+    xhttp.open('GET', 'pages/' + page + '.html', true);
+    xhttp.send();
+  }
 });
