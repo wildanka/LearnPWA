1c830680c55b4e8d1fde298834cfba9c82bc539d
WildanKurniadi <danprogramming10@gmail.com>
2019-06-26 21:21:24 +0700

    #9 Nested Promis4e

--- js/latihan.js
+++ js/latihan.js
@@ -11,6 +11,7 @@ var helloWorld = function(name) {
 
 function triggerPromise(input) {
   console.log(helloWorld(input));
+  // output jika null atau empty: You have to provide the name!
 
   helloWorld('Wildan K')
     .then(function(result) {
@@ -22,4 +23,26 @@ function triggerPromise(input) {
       console.log(error);
     });
   // output: Wildan K
+}
+
+function addMarketkoin(message) {
+  return message + ' from Marketkoingit!';
+}
+
+/**
+ * Seperti yang telah dijelaskan sebelumnya, ada kalanya kita ingin mengeksekusi method lain. Setelah proses pertama, kita dapat gunakan lagi then() setelah then() yang pertama. Misal seperti ini:
+ */
+function nestingThen() {
+  helloWorld('Wildan Si Tamvan')
+    .then(function(result) {
+      // logic for processing first message here.
+      return result;
+    })
+    .then(function(result2) {
+      var message = addMarketkoin(result2);
+      console.log(message);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
