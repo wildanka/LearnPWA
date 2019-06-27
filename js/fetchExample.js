b695599b8c0bda9d30670f286f1ba9cbc178c550
WildanKurniadi <danprogramming10@gmail.com>
2019-06-27 09:27:52 +0700

    #12 Chaining Promise with fetchAPI

--- js/fetchExample.js
+++ js/fetchExample.js
@@ -28,5 +28,28 @@ fetch('https://readerapi.codepolitan.com/articles')
     console.log('Error : ' + error);
   });
 
-
-  
+fetch('https://readerapi.codepolitan.com/articles')
+  .then(function(response) {
+    console.log('============================= Chaining Promise =============');
+    if (response.status !== 200) {
+      console.log('Error : ' + response.status);
+      // Method reject() akan membuat blok catch terpanggil
+      return Promise.reject(new Error(response.statusText));
+    } else {
+      // Promise.resolve akan Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
+      return Promise.resolve(response);
+    }
+  })
+  .then(function(response) {
+    // Mengembalikan sebuah Promise berupa objek/array JavaScript
+    // yang diubah dari teks JSON.
+    return response.json();
+  })
+  .then(function(data) {
+    // Objek/array JavaScript dari response.json() masuk lewat data.
+    console.log(data);
+  })
+  .catch(function(error) {
+    // Parameter error berasal dari Promise.reject()
+    console.log('Error : ' + error);
+  });
