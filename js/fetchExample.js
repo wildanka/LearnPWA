faf2c6b3dbc9a21cf5cec4bc992e236058df919b
WildanKurniadi <danprogramming10@gmail.com>
2019-06-27 09:29:58 +0700

    #13 separate promise method in promise chaining

--- js/fetchExample.js
+++ js/fetchExample.js
@@ -53,3 +53,42 @@ fetch('https://readerapi.codepolitan.com/articles')
     // Parameter error berasal dari Promise.reject()
     console.log('Error : ' + error);
   });
+
+// region separate promise method
+function status(response) {
+  if (response.status !== 200) {
+    console.log('Error : ' + response.status);
+    // Method reject() akan membuat blok catch terpanggil
+    return Promise.reject(new Error(response.statusText));
+  } else {
+    // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
+    return Promise.resolve(response);
+  }
+}
+function json(response) {
+  // Mengembalikan sebuah Promise berupa objek/array JavaScript
+  // yang diubah dari teks JSON.
+  return response.json();
+}
+function error(error) {
+  // Parameter error berasal dari Promise.reject()
+  console.log('Error : ' + error);
+}
+fetch('https://readerapi.codepolitan.com/articles')
+  .then(status)
+  .then(json)
+  .then(function(data) {
+    // Objek/array JavaScript dari response.json() masuk lewat data.
+    console.log(data);
+  })
+  .catch(error);
+// endregion separate promise method
+
+/**
+ * References : 
+ * https://davidwalsh.name/fetch
+https://developers.google.com/web/updates/2015/03/introduction-to-fetch
+https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api
+https://alligator.io/js/fetch-api/ https://developers.google.com/web/fundamentals/primers/service-workers/
+https://github.com/deanhume/progressive-web-apps-book/blob/master/chapter-3/progressive-times/sw.js
+ */
